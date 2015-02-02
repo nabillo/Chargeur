49a7252097ad518277677c5d66725e73fe25ff48
nabillo <nabillo@PC-NABILLO>
2015-02-02 19:37:26 +0000

    initial

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# C18_18F2550 configuration
+CND_ARTIFACT_DIR_C18_18F2550=dist/C18_18F2550/production
+CND_ARTIFACT_NAME_C18_18F2550=Chargeur.X.production.hex
+CND_ARTIFACT_PATH_C18_18F2550=dist/C18_18F2550/production/Chargeur.X.production.hex
+CND_PACKAGE_DIR_C18_18F2550=${CND_DISTDIR}/C18_18F2550/package
+CND_PACKAGE_NAME_C18_18F2550=chargeur.x.tar
+CND_PACKAGE_PATH_C18_18F2550=${CND_DISTDIR}/C18_18F2550/package/chargeur.x.tar
